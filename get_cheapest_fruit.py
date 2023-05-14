fc0e42373c5fc3f9ed617cf191a64585df365bb8
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-14 20:20:17 +0500

    2

--- get_cheapest_fruit.py
+++ get_cheapest_fruit.py
@@ -1,3 +1,4 @@
+from csv import reader
 def get_cheapest_fruit(data: str) -> str:
     """
     This function returns the name of the cheapest fruit
@@ -8,4 +9,14 @@ def get_cheapest_fruit(data: str) -> str:
         str: name of the cheapest fruit
     """
     # your code here
-    pass
+    f=open('fruits.csv')
+    r=reader(f,delimiter=',')
+    m=100000
+    s=''
+    for i in r:
+        if i[1]!='price' and m>float(i[1]):
+            m=float(i[1])
+            s=i[0]
+    return s
+data=str
+print(get_cheapest_fruit(data))
